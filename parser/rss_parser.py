ce12dc1ae3010929cd4f1d7a3b8e93ea87c4d988
William Heavin <william.james.heavin@gmail.com>
2019-06-14 13:15:20 +0100

    Added RSS feed parser

---
+++ parser/rss_parser.py
@@ -0,0 +1,62 @@
+import feedparser
+
+
+class RssUrlParser:
+
+    def __init__(self, url):
+        self.url = url
+
+    def parse(self):
+        rss_feed = feedparser.parse(self.url)
+        self._validate_response(rss_feed)
+        return self._create_rss_channel(rss_feed)
+
+    def _validate_response(self, rss_feed):
+        if not rss_feed:
+            raise RssParserError("Invalid response returned for URL %s" % self.url)
+        if 'bozo_exception' in rss_feed:
+            raise RssParserError("Error parsing for URL %s" % self.url)
+
+    def _create_rss_channel(self, rss_feed):
+        feed = rss_feed['feed']
+        title = feed['title']
+        link = feed['link']
+        description = feed['description']
+        items = rss_feed['entries']
+        return RssChannel(title, link, description, items)
+
+
+class RssChannel:
+
+    def __init__(self, title, url, description, items):
+        self.title = title
+        self.url = url
+        self.description = description
+        self.rss_items = self._parse_items(items)
+
+    def _parse_items(self, items):
+        rss_items = []
+        for item in items:
+            title = item['title']
+            link = item['link']
+            summary = item['summary']
+            published = item['published']
+            author = item['author']
+            rss_items.append(RssItem(title, link, summary, published, author))
+        return rss_items
+
+
+class RssItem:
+
+    def __init__(self, title, link, summary, published, author):
+        self.title = title
+        self.link = link
+        self.summary = summary
+        self.published = published
+        self.author = author
+
+
+class RssParserError(Exception):
+
+    def __init__(self, message):
+        self.message = message
---
+++ tests/parsing/test_rss_parser.py
@@ -0,0 +1,73 @@
+import json
+import unittest
+from unittest.mock import patch
+
+from parser.rss_parser import RssUrlParser, RssParserError
+
+
+class TestRssParsing(unittest.TestCase):
+
+    @patch('feedparser.parse')
+    def test_parse_rss(self, mock_response):
+        # Given: an RSS feed URL to be parsed
+        url = "https://www.fiercewireless.com/rss/xml"
+        obj_under_test = RssUrlParser(url)
+
+        test_data = FileReader("./data/rss_sample.json")
+        mock_response.return_value = test_data.read()
+
+        # When: the URL is parsed
+        rss_channel = obj_under_test.parse()
+
+        # Then: the channel details are returned
+        self.assertEqual("FierceWireless", rss_channel.title)
+        self.assertEqual(url, rss_channel.url)
+
+        # And: the Item details are returned
+        self.assertEqual(2, len(rss_channel.rss_items))
+        first_item = rss_channel.rss_items[0]
+        self.assertIn("Industry Voices—Pongratz", first_item.title)
+        self.assertEqual("Stefan Pongratz", first_item.author)
+
+    def test_parse_for_empty_url(self):
+        # Given: an empty RSS feed URL to be parsed
+        obj_under_test = RssUrlParser("")
+
+        # When: the URL is parsed
+        with self.assertRaises(RssParserError) as error:
+            obj_under_test.parse()
+        self.assertIn("Error parsing for URL", str(error.exception))
+
+    @patch('feedparser.parse')
+    def test_parse_for_empty_response(self, mock_response):
+        # Given: an RSS feed URL to be parsed
+        url = "https://www.fiercewireless.com/rss/xml"
+        obj_under_test = RssUrlParser(url)
+        mock_response.return_value = ""
+
+        # When: the URL is parsed
+        with self.assertRaises(RssParserError) as error:
+            obj_under_test.parse()
+        self.assertIn("Invalid response returned for URL", error.exception.message)
+
+    @patch('feedparser.parse')
+    def test_parse_for_null_response(self, mock_response):
+        # Given: an RSS feed URL to be parsed
+        url = "https://www.fiercewireless.com/rss/xml"
+        obj_under_test = RssUrlParser(url)
+        mock_response.return_value = None
+
+        # When: the URL is parsed
+        with self.assertRaises(RssParserError) as error:
+            obj_under_test.parse()
+        self.assertIn("Invalid response returned for URL", error.exception.message)
+
+
+class FileReader:
+
+    def __init__(self, path):
+        self.path = path
+
+    def read(self):
+        with open(self.path) as json_file:
+            return json.load(json_file)
