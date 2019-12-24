cab3673e5c0379e1265c4aebfed65f50fde93c30
William <william.james.heavin@gmail.com>
2019-12-24 16:54:26 +0000

    More HTML and CSS updates

---
+++ run_app_debug.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# activate virtual environment
+. env/bin/activate
+
+# run all unit tests
+python3 tester.py
+
+# start app in debug mode
+export FLASK_APP=app.py
+export FLASK_ENV=development
+flask run
